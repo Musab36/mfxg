81147a7fde273e08a7331e4192636e5c3a8b4ee2
Musab36 <musabhd@gmail.com>
2020-01-13 15:01:24 +0200

    custom toolbar for each activity

--- app/src/main/java/com/salajim/musab/mfxg/LessonsActivity.java
+++ app/src/main/java/com/salajim/musab/mfxg/LessonsActivity.java
@@ -1,6 +1,7 @@
 package com.salajim.musab.mfxg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,12 @@ public class LessonsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons);
+
+        // Setting up the custom toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
