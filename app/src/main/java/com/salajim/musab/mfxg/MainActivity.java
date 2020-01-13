8263144d5566eaed3105e6f59cba05f5afb1dcc9
Musab36 <musabhd@gmail.com>
2020-01-13 13:53:47 +0200

    replaced the toolbar with a custom one

--- app/src/main/java/com/salajim/musab/mfxg/MainActivity.java
+++ app/src/main/java/com/salajim/musab/mfxg/MainActivity.java
@@ -7,14 +7,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActionBar toolBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolBar = getSupportActionBar();
-        toolBar.setTitle("FOREX GROUP");
     }
 }
