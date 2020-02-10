485b3c5514db01bde015041852e6532bfd0b7013
Musab36 <musabhd@gmail.com>
2020-02-10 13:56:58 +0200

    fixed firebase retrieving data error

--- app/src/main/java/com/salajim/musab/mfxg/adapters/SignalsAdapter.java
+++ app/src/main/java/com/salajim/musab/mfxg/adapters/SignalsAdapter.java
@@ -1,4 +1,74 @@
 package com.salajim.musab.mfxg.adapters;
 
-public class SignalsAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.salajim.musab.mfxg.R;
+import com.salajim.musab.mfxg.models.Signals;
+
+import java.util.List;
+
+public class SignalsAdapter extends RecyclerView.Adapter<SignalsAdapter.SignalsViewHolder> {
+    private List<Signals> signals;
+    private Context mContext;
+
+    public SignalsAdapter(Context context, List<Signals> signals) {
+        this.mContext = context;
+        this.signals = signals;
+    }
+
+    @Override
+    public SignalsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.signals_view, parent, false);
+
+        return new SignalsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SignalsViewHolder holder, int position) {
+        holder.bindSignals(signals.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return signals.size();
+    }
+
+    public class SignalsViewHolder extends RecyclerView.ViewHolder {
+        private Context mContext;
+
+        TextView pairs;
+        TextView signal;
+        TextView entry;
+        TextView takeProfit;
+        TextView stopLoss;
+
+        public SignalsViewHolder(View itemView) {
+            super(itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindSignals(Signals signals) {
+            pairs = (TextView) itemView.findViewById(R.id.pairText);
+            signal = (TextView) itemView.findViewById(R.id.signalText);
+            entry = (TextView) itemView.findViewById(R.id.entryText);
+            takeProfit = (TextView) itemView.findViewById(R.id.profitText);
+            stopLoss = (TextView) itemView.findViewById(R.id.stopLossText);
+
+            pairs.setText("Pair: " + signals.getPair());
+            signal.setText("Signal: " + signals.getSignal());
+            entry.setText("Entry: " + signals.getEntry());
+            takeProfit.setText("Take Profit: " + signals.getTakeProfit());
+            stopLoss.setText("Stop Loss: " + signals.getStopLoss());
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/salajim/musab/mfxg/ui/SignalsActivity.java
+++ app/src/main/java/com/salajim/musab/mfxg/ui/SignalsActivity.java
@@ -1,17 +1,40 @@
 package com.salajim.musab.mfxg.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.salajim.musab.mfxg.R;
+import com.salajim.musab.mfxg.adapters.SignalsAdapter;
+import com.salajim.musab.mfxg.models.Signals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SignalsActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+    private ArrayList<Signals> signalsList = new ArrayList<>();
+    private SignalsAdapter mAdapter;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +47,42 @@ public class SignalsActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        recyclerView = (RecyclerView) findViewById(R.id.signalsRecycler);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        // Retrieving data from firebase database
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("signals");
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        displayData();
+    }
+
+    // Retrieving data from firebase
+    private void displayData() {
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    signalsList.add(ds.getValue(Signals.class));
+                }
+
+                mAdapter = new SignalsAdapter(getApplicationContext(), signalsList);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setAdapter(mAdapter);
+
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     // inflating the menu
